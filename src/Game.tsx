9078761bd1378bd4a88a64757865efe5331398f7
Dinh Doan Van Bien <999472+voieducode@users.noreply.github.com>
2020-06-06 20:52:29 +0100

    Reveal on first click

--- src/Game.tsx
+++ src/Game.tsx
@@ -44,6 +44,53 @@ function neighbours(center: number, width: number, height: number) {
   return [left, right, up, down, leftUp, leftDown, rightUp, rightDown];
 }
 
+/*
+Flood-fill (node, target-color, replacement-color):
+ 1. If target-color is equal to replacement-color, return.
+ 2. ElseIf the color of node is not equal to target-color, return.
+ 3. Else Set the color of node to replacement-color.
+ 4. Perform Flood-fill (one step to the south of node, target-color, replacement-color).
+    Perform Flood-fill (one step to the north of node, target-color, replacement-color).
+    Perform Flood-fill (one step to the west of node, target-color, replacement-color).
+    Perform Flood-fill (one step to the east of node, target-color, replacement-color).
+ 5. Return.
+*/
+function reveal(
+  tiles: ITile[],
+  at: number,
+  width: number,
+  height: number,
+  depth = 0
+): ITile[] {
+  const pos = at;
+  const tile = tiles[pos];
+
+  if (tile.revealed) return tiles;
+
+  if (tile.content === "💣" || tile.flagged) return tiles;
+
+  if ([1, 2, 3, 4, 5, 6, 7, 8].includes(tile.content as number)) {
+    tile.display = tile.content.toString();
+    tile.revealed = true;
+  }
+
+  if (tile.content === 0) {
+    tile.display = " ";
+    tile.revealed = true;
+  } else if (depth > 1) {
+    return tiles;
+  }
+
+  const [left, right, up, down] = neighbours(pos, width, height);
+
+  if (down >= 0) reveal(tiles, down, width, height, depth + 1);
+  if (up >= 0) reveal(tiles, up, width, height, depth + 1);
+  if (left >= 0) reveal(tiles, left, width, height, depth + 1);
+  if (right >= 0) reveal(tiles, right, width, height, depth + 1);
+
+  return tiles;
+}
+
 function populateTiles(
   startPosition: number,
   width: number,
@@ -91,7 +138,7 @@ function Mine(props: { tile: ITile; onLeftClick: (pos: number) => void }) {
       value={tile.id}
       onClick={() => props.onLeftClick(tile.id)}
     >
-      {tile.content.toString()}
+      {tile.display}
     </button>
   );
 }
@@ -127,6 +174,20 @@ function MineField(props: {
   );
 }
 
+function Info(props: { mines: number; phase: string }) {
+  return (
+    <div
+      style={{
+        marginLeft: "1em",
+      }}
+    >
+      <p>
+        {props.phase} | 💣 {props.mines}
+      </p>
+    </div>
+  );
+}
+
 export function Game() {
   const width = 9;
   const height = 9;
@@ -136,11 +197,27 @@ export function Game() {
   const [phase, setPhase] = useState("ready");
 
   function initGameAt(startPosition: number) {
-    setTiles(populateTiles(startPosition, width, height, mines));
+    setTiles(
+      reveal(
+        populateTiles(startPosition, width, height, mines),
+        startPosition,
+        width,
+        height,
+        0
+      )
+    );
     setPhase("playing");
   }
 
   return (
-    <MineField tiles={tiles} width={9} phase={phase} initGameAt={initGameAt} />
+    <>
+      <Info mines={mines} phase={phase} />
+      <MineField
+        tiles={tiles}
+        width={9}
+        phase={phase}
+        initGameAt={initGameAt}
+      />
+    </>
   );
 }
